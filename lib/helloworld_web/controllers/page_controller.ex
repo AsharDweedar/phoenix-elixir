38de8321a7838fd3272eb51a0328fbf23d17205e
As-har Dweedar <ashar.dweedar@gmail.com>
2017-12-17 16:38:21 +0200

    (add) go to home page using page_path helper

--- lib/helloworld_web/controllers/page_controller.ex
+++ lib/helloworld_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule HelloworldWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+  def postme(conn, params) do
+    IO.puts "params"
+    IO.puts params
+    render conn, "<h1> posted </h1>"
+  end
 end
--- lib/helloworld_web/router.ex
+++ lib/helloworld_web/router.ex
@@ -14,12 +14,27 @@ defmodule HelloworldWeb.Router do
   end
 
   scope "/", HelloworldWeb do
-    pipe_through :browser # Use the default browser stack
-
+    pipe_through :browser # Use the default browser stack, these are for middlewares 
+    """
+    # this one will have to pass auth midware
+    # all authenticate_user pipelines will be called on this request before being forwarded 
+    scope "/" do
+      pipe_through [:authenticate_user, :ensure_admin]
+      forward "/jobs", BackgroundJob.Plug # can pass args here :(, name : "ashar")
+    end
+    """
+    post "/", PageController, :postme
     get "/", PageController, :index
+  end
+  scope "/custom", HelloworldWeb do
+    pipe_through :browser
+
     get "/custom", CustomController, :custom
     get "/custom/another", CustomController, :another
     get "/custom/:name", CustomController, :msg
+    # get "/", RootController, :index #will never get excuted => in order the line above will take the rout and the search for other matches will stop
+    resources "/users", UserController, only: [:index, :edit] #select some of the standard routes that comes with it 
+    resources "/posts", UserController, except: [:delete] #select all the standard routes that comes with it except of delete
   end
  
 
