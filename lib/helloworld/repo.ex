be581fb93e86aefe287eb1c1f6595accb957634e
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-12-17 10:15:14 +0200

    init

---
+++ lib/helloworld/repo.ex
@@ -0,0 +1,11 @@
+defmodule Helloworld.Repo do
+  use Ecto.Repo, otp_app: :helloworld
+
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
+  """
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
+end
