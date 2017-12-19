bf88c8a8c67fd642228a5e2acca09c94e766c351
As-har Dweedar <ashar.dweedar@gmail.com>
2017-12-19 09:53:18 +0200

    (add) flash message to endpoint /custom/anither

--- lib/helloworld_web/controllers/custom_controller.ex
+++ lib/helloworld_web/controllers/custom_controller.ex
@@ -1,11 +1,13 @@
 defmodule HelloworldWeb.CustomController do
   use HelloworldWeb, :controller
-
   def custom(conn, _params) do
     render conn, "custom.html", greet: "hi again"
   end
   def another(conn, _params) do
-    render conn, "another.html"
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("another.html")
   end
   def msg(conn, %{"name" => name}) do
     greet = "hi : #{name}"
